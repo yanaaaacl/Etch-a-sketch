85314dbf00a1a5c1a7f3bdd2a99cff826c970054
Yana Chackhovskaya <a9506862137@yandex.ru>
2023-11-03 17:20:52 +0300

    square add

--- js/main.js
+++ js/main.js
@@ -3,20 +3,27 @@ let rangeNumbers = document.querySelector('#numbers__range');
 
 let squareGrid = document.querySelector('.square');
 
-let doubleValue = rangeNumbers.value * rangeNumbers.value;
-let smallSquare = document.createElement('div');
-smallSquare.className
-for (let i = 0; i <= doubleValue; i++){
-    let 
-}
 
 
-function changeNumbers(){
+
+
+
+
+function changeNumbers() {
     textNumbers.textContent = rangeNumbers.value + 'X' + rangeNumbers.value;
     squareGrid.style.gridTemplateColumns = `repeat(${rangeNumbers.value}, 1fr)`;
     squareGrid.style.gridTemplateRows = `repeat(${rangeNumbers.value}, 1fr)`;
+    for (let i = 0; i < rangeNumbers.value * rangeNumbers.value; i++) {
+        let smallSquare = document.createElement('div');
+        smallSquare.classList.add('small');
+        smallSquare.addEventListener('click', function colort(){
+            smallSquare.style.backgroundColor = 'black';
+        });
+        
+        squareGrid.appendChild(smallSquare);
+        
+    }
 }
-
 
 
 rangeNumbers.addEventListener('mousemove', changeNumbers);
